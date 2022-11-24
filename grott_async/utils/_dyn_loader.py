751b2e31492f4ddad353ced526676de211b2408e
githuDante <github@dante.tk>
2022-11-24 14:35:25 +0200

    Plugins loader
    Changed attribute names for better handling in the proxy
    Current dir added to sys.path - needed for proper loading of plugins

--- grott_async/utils/_dyn_loader.py
+++ grott_async/utils/_dyn_loader.py
@@ -1,7 +1,9 @@
 import inspect
 import os
+import sys
 from importlib import import_module
 from logging import getLogger
+from typing import Dict
 from grott_async.extras.plugin import GrottProxySyncPlugin, GrottProxyAsyncPlugin
 
 log = getLogger('grott')
@@ -15,28 +17,31 @@ class GrottPluginLoader:
     __plugin_dir__ = 'plugins'
 
     def __init__(self):
-        self.plugins = {}
-        self.async_plugins = {}
+        self.sync_plugins: Dict[str, GrottProxySyncPlugin] = {}
+        self.async_plugins: Dict[str, GrottProxyAsyncPlugin] = {}
         self.load()
 
     def load(self):
         if os.path.exists(self.__plugin_dir__) is False:
             log.info('Plugins dir missing.')
             return
+        work_path = os.path.abspath(os.curdir)
+        if work_path not in sys.path:
+            sys.path.insert(1, work_path)
         for f in os.listdir(self.__plugin_dir__):
             if f.endswith('.py'):
                 mod_name = f.split('.py')[0]
                 try:
                     mod = import_module(f'{self.__plugin_dir__}.{mod_name}')
-                except Exception:
-                    log.error(f'Loading failed for {mod_name}')
+                except Exception as e:
+                    log.error(f'Loading failed for {mod_name} : {e}')
                     continue
                 log.info(f'Checking for plugins in {mod_name}')
                 sync_plugins = [(x, y) for x, y in inspect.getmembers(mod) if isinstance(y, GrottProxySyncPlugin)]
                 async_plugins = [(x, y) for x, y in inspect.getmembers(mod) if isinstance(y, GrottProxyAsyncPlugin)]
-                log.info(f'Found: {len(sync_plugins) + len(async_plugins)} in {mod_name}')
+                log.info(f'Found: {len(sync_plugins) + len(async_plugins)} plugin in {mod_name}')
                 for plugin in sync_plugins:
-                    self.plugins.update({plugin[0]: plugin[1]})
+                    self.sync_plugins.update({plugin[0]: plugin[1]})
                 for plugin in async_plugins:
                     self.async_plugins.update({plugin[0]: plugin[1]})
-        log.info(f'Loaded [{len(self.plugins) + len(self.async_plugins)}] plugins.')
+        log.info(f'Loaded [{len(self.sync_plugins) + len(self.async_plugins)}] plugins total.')
